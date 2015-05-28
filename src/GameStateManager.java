47a0ffcd6c595cf8bd9ccce850eb11e2a42f5d6c
Atlowell <atlowell42@gmail.com>
2015-05-27 23:21:26 -0400

    Fixed Menu Texture

--- src/GameStateManager.java
+++ src/GameStateManager.java
@@ -243,13 +243,13 @@ public class GameStateManager {
 		}
 
 		GL11.glBegin(GL11.GL_QUADS);
-			GL11.glTexCoord2f(0, 0);
+			GL11.glTexCoord2f(0, background.getHeight());
 			GL11.glVertex2f(0, height);
-			GL11.glTexCoord2f(1, 0);
+			GL11.glTexCoord2f(background.getWidth(), background.getHeight());
 			GL11.glVertex2f(width, height);
-			GL11.glTexCoord2f(1, 1);
+			GL11.glTexCoord2f(background.getWidth(), 0);
 			GL11.glVertex2f(width, 0);
-			GL11.glTexCoord2f(0, 1);
+			GL11.glTexCoord2f(0, 0);
 			GL11.glVertex2f(0, 0);
 		GL11.glEnd();
 
